40687623cc2b04bf9f0fe5297f7349da00fde07d
wiggleji <roamgom@gmail.com>
2022-06-09 13:18:09 +0900

    apply spring hateoas on employee controller

--- tutorial/src/main/java/com/example/tutorial/payroll/controller/EmployeeController.java
+++ tutorial/src/main/java/com/example/tutorial/payroll/controller/EmployeeController.java
@@ -21,8 +21,17 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+        // 각 employee entity에 대해 one과 all을 link로 넣어 collector list로 반환하고
+        // collectionmodel에 all에 대한 self relation을 적용하여 return 한다
+        List<EntityModel<Employee>> employees = repository.findAll().stream().map(employee -> EntityModel.of(
+                employee,
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"))
+        ).collect(Collectors.toList());
+
+        return CollectionModel.of(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
@@ -31,12 +40,13 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return repository.findById(id)
+    EntityModel<Employee> one(@PathVariable Long id) {
+        Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
-
-
 
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
