4118c635d3de51f5161cb1afbe900821ec075846
wiggleji <roamgom@gmail.com>
2022-06-09 17:06:13 +0900

    apply ResponseEntity to EmployeeController & divide Employee Entity name

--- tutorial/src/main/java/com/example/tutorial/payroll/controller/EmployeeController.java
+++ tutorial/src/main/java/com/example/tutorial/payroll/controller/EmployeeController.java
@@ -7,6 +7,9 @@ import com.example.tutorial.payroll.repository.EmployeeRepository;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,16 +32,19 @@ public class EmployeeController {
         // 각 employee entity에 대해 one과 all을 link로 넣어 collector list로 반환하고
         // collectionmodel에 all에 대한 self relation을 적용하여 return 한다
         List<EntityModel<Employee>> employees = repository.findAll().stream().map(
-                assembler::toModel
-        ).collect(Collectors.toList());
+                assembler::toModel).collect(Collectors.toList());
 
         return CollectionModel.of(employees,
                 linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
-    Employee newEmployee(@RequestBody Employee newEmployee) {
-        return repository.save(newEmployee);
+    ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) {
+        EntityModel<Employee> entityModel = assembler.toModel(repository.save(newEmployee));
+
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
     @GetMapping("/employees/{id}")
@@ -49,22 +55,34 @@ public class EmployeeController {
     }
 
     @PutMapping("/employees/{id}")
-    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+    ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
 
-        return repository.findById(id)
+        Employee updatedEmployee = repository.findById(id)
                 .map(employee -> {
                     employee.setName(newEmployee.getName());
                     employee.setRole(newEmployee.getRole());
                     return repository.save(employee);
                 })
+                // 이 로직에선 id 값으로 조회한 객체에 대해 수정이 이루어지고
+                // 존재하지 않을 경우 새로 만들어주는 로직이지만
+                // 이는 서비스 성격에 맞추어 에러 처리를 해줘도 된다.
                 .orElseGet(() -> {
                     newEmployee.setId(id);
                     return repository.save(newEmployee);
                 });
+
+        EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+
+        // created로 인해 이미 존재한 데이터에 PUT으로 수정시에도 201 created로 처리됌
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
     @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
         repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
