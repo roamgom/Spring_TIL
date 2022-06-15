be08f26b4b121f619a4be9227a703a45910d5a0b
wiggleji <roamgom@gmail.com>
2022-06-15 11:38:12 +0900

    add tutorial member&order service w. only Java

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,29 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        // 멤버와 할인의 대한 책임을 각각 memberRepository와 discountPolicy에 위임하여 처리하기 때문에
+        // OrderSerivce에서는 별도로 신경쓰지 않아도 된다. -> 단일책임원칙을 잘 지킴.
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
