f3c89b0eacceb854dc40cb8f93d87fccd63f2e15
wiggleji <roamgom@gmail.com>
2022-06-22 00:02:34 +0900

    add Code for Singleton & AppConfig & ComponentScan w.TestCase

--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     // 동시성 문제로 실무에선 ConcurrencyHashmap을 사용
